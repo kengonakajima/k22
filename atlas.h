e5ccf2b918efd576e45619e5ed9a11a25f737637
Kengo Nakajima <kengo.nakajima@gmail.com>
2016-03-22 13:23:34 +0900

    sprite index defs

---
+++ atlas.h
@@ -0,0 +1,65 @@
+﻿#ifndef _ATLAS_H_
+#define _ATLAS_H_
+
+#define AU 32
+
+// etc
+#define B_ATLAS_WHITEBOX (35*AU+20)
+
+// Spaceship grounds
+#define B_ATLAS_SHIP_HATCH_BASE (33*AU+8)
+#define B_ATLAS_SHIP_WINDOW (30*AU+2)
+
+
+
+// PC indexes
+#define B_ATLAS_PC_BODY_BASE (31*AU+24)
+#define B_ATLAS_PC_FACE_FRONT (30*AU+24)
+#define B_ATLAS_PC_FACE_LEFT (30*AU+25)
+#define B_ATLAS_PC_HAIR_FRONT (29*AU+24)
+#define B_ATLAS_PC_HAIR_LEFT (29*AU+25)
+#define B_ATLAS_PC_HAIR_BACK (29*AU+26)
+#define B_ATLAS_PC_EQUIPMENT_BEAMGUN_BASE (26*AU+0)
+
+#define B_ATLAS_WIDE_BEAM (31*AU+20)
+#define B_ATLAS_LASER_BEAM (33*AU+20)
+#define B_ATLAS_RIPPLE_BEAM (34*AU+20)
+
+
+// Hint arrows
+#define B_ATLAS_UP_ARROW (28*AU+9)
+#define B_ATLAS_RIGHT_ARROW (28*AU+10)
+#define B_ATLAS_DOWN_ARROW (28*AU+11)
+#define B_ATLAS_LEFT_ARROW (28*AU+12)
+
+
+// Powerups
+#define B_ATLAS_LIGHTBLUE_CIRCLE (27*AU+0)
+#define B_ATLAS_LIGHTGREEN_CIRCLE (27*AU+1)
+#define B_ATLAS_ORANGE_CIRCLE (27*AU+2)
+#define B_ATLAS_RED_CIRCLE (27*AU+3)
+#define B_ATLAS_WAVE_PROGRESS (36*AU+20)
+#define B_ATLAS_WAVE_REGRESS (36*AU+21)
+
+// Enemies
+#define B_ATLAS_TAKWASHI (37*AU+10)
+#define B_ATLAS_FLY_BASE (18*AU+9)
+#define B_ATLAS_REPAIRER_BASE (39*AU+9)
+#define B_ATLAS_BUILDER_BASE (41*AU+9)
+#define B_ATLAS_MINE (38*AU+9)
+#define B_ATLAS_FLYGEN (18*AU+11)
+
+// Bullets
+#define B_ATLAS_BULLET_BOUNCER (37*AU+9)
+#define B_ATLAS_SPARIO (38*AU+11)
+#define B_ATLAS_HYPER_SPARIO (38*AU+12)
+#define B_ATLAS_SIG (36*AU+12)
+#define B_ATLAS_GOOBALL_BASE (19*AU+0)
+
+// Effects
+#define B_ATLAS_EXPLOSION (37*AU+20)
+
+// ascii atlas
+#define A_ATLAS_BIG_DIGIT_START (32+16)
+
+#endif
